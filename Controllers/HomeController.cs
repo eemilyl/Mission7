0738227e14762496d550ef30ef37c38d64aa7149
eemilyl <eevett7@gmail.com>
2022-02-16 08:42:35 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Bookstore.Models;
+using Bookstore.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bookstore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IBookstoreRepository repo;
+        public HomeController (IBookstoreRepository temp)
+        {
+            repo = temp;
+        }
+        public IActionResult Index(int pageNum = 1)
+        {
+            int numResults = 10;
+            var b = new BooksViewModel
+            {
+                Books = repo.Books
+                .OrderBy(b => b.Title)
+                .Skip((pageNum - 1) * numResults)
+                .Take(numResults),
+
+                PageInfo = new PageInfo
+                {
+                    TotalNumBooks = repo.Books.Count(),
+                    BooksPerPage = numResults,
+                    CurrentPage = pageNum
+                }
+            };
+           
+            return View(b);
+        }
+
+
+    }
+}
