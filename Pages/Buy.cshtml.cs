a666b7b49fadb4a1a46054f2dee2e4e7e14f6ccc
eemilyl <eevett7@gmail.com>
2022-02-23 22:00:07 -0700

    basket before sessions

--- Pages/Buy.cshtml.cs
+++ Pages/Buy.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Bookstore.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,22 @@ namespace Bookstore.Pages
 {
     public class BuyModel : PageModel
     {
-        public void OnGet()
+        private IBookstoreRepository repo { get; set; }
+        public BuyModel (IBookstoreRepository temp)
         {
+            repo = temp;
+        }
+        public Basket basket { get; set; }
+        public void OnGet(Basket b)
+        {
+            basket = b;
+        }
+        public IActionResult OnPost(int BookId)
+        {
+            Book b = repo.Books.FirstOrDefault(x => x.BookId == BookId);
+            basket = new Basket();
+            basket.AddItem(b, 1);
+            return RedirectToPage(basket);
         }
     }
 }
